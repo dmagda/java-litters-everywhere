a6b52896f46b2fdf73774f603aa46696e38f0656
Denis Magda <dmagda@gridgain.com>
2022-11-15 16:41:06 -0500

    added a few tests

--- README.md
+++ README.md
@@ -432,7 +432,7 @@ YugabyteDB is an LSM-tree based database. It uses another approach to keep track
     docker run -d --name yugabytedb_node1 --net yugabytedb_network \
     -p 7001:7000 -p 9000:9000 -p 5433:5433 \
     -v ~/yb_docker_data/node1:/home/yugabyte/yb_data --restart unless-stopped \
-    yugabytedb/yugabyte:latest \
+    yugabytedb/yugabyte:2.14.4.0-b26 \
     bin/yugabyted start --listen=yugabytedb_node1 \
     --base_dir=/home/yugabyte/yb_data --daemon=false
     ```
--- src/test/java/com/yugabytedb/example/java/JavaLittersEverywhereApplicationTests.java
+++ src/test/java/com/yugabytedb/example/java/JavaLittersEverywhereApplicationTests.java
@@ -1,13 +1,72 @@
 package com.yugabytedb.example.java;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.yugabytedb.example.java.data.PizzaOrder;
+import com.yugabytedb.example.java.data.PizzaOrdersRepository;
+import com.yugabytedb.example.java.data.PizzaOrder.OrderStatus;
+
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class JavaLittersEverywhereApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    PizzaOrdersRepository repository;
 
+    @Test
+    @Order(1)
+    void contextLoads() {
+    }
+
+    @Test
+    @Order(2)
+    void putNewOrder() {
+        PizzaOrder pizzaOrder = new PizzaOrder();
+        pizzaOrder.setId(1);
+        pizzaOrder.setStatus(OrderStatus.Ordered);
+
+        repository.save(pizzaOrder);
+    }
+
+    @Test
+    @Order(3)
+    void getPizzaOrder() {
+        Optional<PizzaOrder> orderOptional = repository.findById(1);
+
+        assert orderOptional.isPresent();
+
+        PizzaOrder pizzaOrder = orderOptional.get();
+
+        System.out.println(pizzaOrder);
+    }
+    
+    @Test
+    @Order(4)
+    void updateOrderStatus() {
+        Optional<PizzaOrder> orderOptional = repository.findById(1);
+
+        assert orderOptional.isPresent();
+
+        PizzaOrder pizzaOrder = orderOptional.get();
+        
+        
+        pizzaOrder.setStatus(OrderStatus.Baking);
+        
+        repository.save(pizzaOrder);
+    }
+
+    @Test
+    @Order(5)
+    void deletePizzaOrder() {
+        repository.delete(repository.getReferenceById(1));
+
+        assert repository.findById(1).isEmpty();
+    }
 }
